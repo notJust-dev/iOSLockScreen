a5947a20b69e218b6546184fa3d08a20995ac2ab
Vadim Savin <savinvadim1312@gmail.com>
2022-10-07 16:09:38 +0100

    Sensor Animated Image

--- App.js
+++ App.js
@@ -1,118 +1,24 @@
-import { useState, useEffect, useMemo } from "react";
+import { View, StyleSheet } from "react-native";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ImageBackground } from "react-native";
-import wallpaper from "./assets/images/wallpaper.webp";
-import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
-import dayjs from "dayjs";
-import NotificationsList from "./src/components/NotificationsList";
-import SwipeUpToOpen from "./src/components/SwipeUpToOpen";
+import bg from "./assets/images/bg.jpeg";
+import SensorAnimatedImage from "./src/components/SensorAnimatedImage";
 
-import Animated, {
-  SlideInDown,
-  SlideInUp,
-  useSharedValue,
-  useAnimatedStyle,
-  interpolate,
-  useDerivedValue,
-} from "react-native-reanimated";
-
-export default function App() {
-  const [date, setDate] = useState(dayjs());
-
-  const footerVisibility = useSharedValue(1);
-  const footerHeight = useDerivedValue(() =>
-    interpolate(footerVisibility.value, [0, 1], [0, 85])
-  );
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setDate(dayjs());
-    }, 1000 * 60);
-
-    return () => clearInterval(interval);
-  }, []);
-
-  const animatedFooterStyle = useAnimatedStyle(() => ({
-    marginTop: interpolate(footerVisibility.value, [0, 1], [-85, 0]),
-    opacity: footerVisibility.value,
-  }));
-
-  const Header = useMemo(
-    () => (
-      <Animated.View entering={SlideInUp} style={styles.header}>
-        <Ionicons name="ios-lock-closed" size={20} color="white" />
-        <Text style={styles.date}>{date.format("dddd, DD MMMM")}</Text>
-        <Text style={styles.time}>{date.format("hh:mm")}</Text>
-      </Animated.View>
-    ),
-    [date]
-  );
-
+const App = () => {
   return (
-    <ImageBackground source={wallpaper} style={styles.container}>
-      {/* Notification List */}
-      <NotificationsList
-        footerVisibility={footerVisibility}
-        footerHeight={footerHeight}
-        ListHeaderComponent={Header}
-      />
+    <View style={styles.container}>
+      <SensorAnimatedImage image={bg} />
 
-      <Animated.View
-        entering={SlideInDown}
-        style={[styles.footer, animatedFooterStyle]}
-      >
-        <View style={styles.icon}>
-          <MaterialCommunityIcons name="flashlight" size={24} color="white" />
-        </View>
-
-        <SwipeUpToOpen />
-
-        <View style={styles.icon}>
-          <Ionicons name="ios-camera" size={24} color="white" />
-        </View>
-      </Animated.View>
-
-      <StatusBar style="light" />
-    </ImageBackground>
+      <StatusBar style="dark" />
+    </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "stretch",
-  },
-  header: {
     alignItems: "center",
     justifyContent: "center",
-    height: 250,
-  },
-  date: {
-    color: "#C3FFFE",
-    fontSize: 20,
-    fontWeight: "500",
-    marginTop: 20,
-  },
-  time: {
-    fontSize: 82,
-    fontWeight: "bold",
-    color: "#C3FFFE",
-  },
-  footer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    marginTop: "auto",
-    marginBottom: 10,
-    paddingVertical: 10,
-    paddingHorizontal: 30,
-    height: 75,
-  },
-  icon: {
-    backgroundColor: "#00000050",
-    width: 50,
-    aspectRatio: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    borderRadius: 50,
   },
 });
+
+export default App;
---
+++ src/components/SensorAnimatedImage.js
@@ -0,0 +1,48 @@
+import { useWindowDimensions } from "react-native";
+import Animated, {
+  useAnimatedSensor,
+  SensorType,
+  useAnimatedStyle,
+  interpolate,
+  withTiming,
+} from "react-native-reanimated";
+
+const IMAGE_OFFSET = 100;
+const PI = Math.PI;
+const HALF_PI = PI / 2;
+
+const SensorAnimatedImage = ({ image }) => {
+  const { width, height } = useWindowDimensions();
+
+  const sensor = useAnimatedSensor(SensorType.ROTATION);
+
+  const imageStyle = useAnimatedStyle(() => {
+    const { pitch, roll } = sensor.sensor.value;
+
+    return {
+      top: withTiming(
+        interpolate(pitch, [-HALF_PI, HALF_PI], [-IMAGE_OFFSET * 2, 0]),
+        { duration: 100 }
+      ),
+      left: withTiming(interpolate(roll, [-PI, PI], [-IMAGE_OFFSET * 2, 0]), {
+        duration: 100,
+      }),
+    };
+  });
+
+  return (
+    <Animated.Image
+      source={image}
+      style={[
+        {
+          width: width + 2 * IMAGE_OFFSET,
+          height: height + 2 * IMAGE_OFFSET,
+          position: "absolute",
+        },
+        imageStyle,
+      ]}
+    />
+  );
+};
+
+export default SensorAnimatedImage;
